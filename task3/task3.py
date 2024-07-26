9d787ad96591f63e8ce0e30b3e4a81b924438b29
makel0ve <104302350+makel0ve@users.noreply.github.com>
2024-07-26 15:30:21 +0400

    Add files via upload

---
+++ task3/task3.py
@@ -0,0 +1,56 @@
+import json
+import sys
+
+
+def list_to_dict(t):
+    for i in t:
+        if i.get("value") == '':
+            i["value"] = get_value(i.get("id"))
+            
+        if i.get("values"):
+            list_to_dict(i.get("values"))
+
+
+def get_value(id):
+    for val in v:
+        if val.get("id") == id:
+            return val.get("value")
+
+
+def main(a, b, c):
+    global v
+    if not a and not b and not c:
+        print("Введите путь к файлу values.json:")
+        a = input().replace('"', '')
+        print("\nВведите путь к файлу tests.json:")
+        b = input().replace('"', '')
+        print("\nВведите путь к файлу report.json:")
+        c = input().replace('"', '')
+    else:
+        a = a
+        b = b
+        c = c
+        
+    f1 = open(a, 'r')
+    f2 = open(b, 'r')
+    f3 = open(c, 'w')
+
+    values = json.load(f1)
+    test = json.load(f2)
+    t = test.get("tests")
+    v = values.get("values")
+    
+    list_to_dict(t)
+    
+    json.dump(test, f3, indent=2)
+    f1.close()
+    f2.close()
+    f3.close()
+
+    print(f"\nФайл готов и находится\n{c}")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main(None, None, None)
+    else:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
